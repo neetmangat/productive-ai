13c2e5a97d0dd2503be9bf3e764e183712142c0b
neetmangat <neetmangat@gmail.com>
2023-11-17 16:50:09 -0800

    Header cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-[#F5F6F8]">{children}</body>
     </html>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useState } from "react";
-// import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.png";
 import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 
 function Header() {
   const [search, setSearch] = useState("");
+  const [summary, setSummary] = useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,9 +18,9 @@ function Header() {
   return (
     <header className="">
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-[#0055D1] to-[#DC5CF9] rounded-md filter blur-3xl opacity-50 -z-50" />
         {/* Logo */}
-        {/* <div className="flexm-2 p-4 font-lg align-middle"> */}
-        <Image
+        {/* <Image
           // src={logo}
           src={"https://links.papareact.com/c2cdd5"}
           alt="logo"
@@ -27,9 +28,20 @@ function Header() {
           height={100}
           // className="object-contain mx-4 rounded"
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
-        />
-        {/* <h1 className="font-light text-xl tracking-widest">Productive.AI</h1> */}
-        {/* </div> */}
+        /> */}
+
+        <div className="flex items-center pb-10 md:pb-0 object-contain">
+          <Image
+            src={logo}
+            alt="logo"
+            width={50}
+            height={50}
+            className="object-contain mx-4 rounded"
+          />
+          <h1 className="font-extralight text-4xl tracking-widest text-[#2A3757]">
+            Productive.AI
+          </h1>
+        </div>
 
         {/* Right Side */}
         <div className="flex items-center space-x-5 flex-1 justify-end w-full">
@@ -52,14 +64,16 @@ function Header() {
           </form>
 
           {/* Avatar */}
-          <Avatar name="Neet Mangat" size="50" round color="#0055D1" />
+          <Avatar name="Neet Mangat" size="50" round color="#B423F8" />
         </div>
       </div>
 
-      <div className="flex items-center justify-center px-5 md:py-5">
-        <p className=" flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-          GPT is summarizing your tasks for the day...
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+          <div className={`${!summary && "animate-pulse"} flex items-center`}>
+            <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+            Your personal assistant is summarizing the tasks for today...
+          </div>
         </p>
       </div>
     </header>
